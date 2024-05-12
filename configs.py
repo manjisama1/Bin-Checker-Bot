f2d6858bc3db90bc9f8d92cd2f43747ec31380ea
manjisama1 <111729787+manjisama1@users.noreply.github.com>
2024-05-12 12:11:49 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -3,6 +3,6 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "0112234"))
     API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6942558543:AAGRQGTKHw2wT3e-vBE_WN9dBEoAKaw0Ad4")
 
 config = Config()
